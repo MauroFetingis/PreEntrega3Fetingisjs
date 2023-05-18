1c4bdc938d84b605f87f239885c5702390470849
Mauro Fetingis <maurofetingis17@gmail.com>
2023-05-18 20:36:50 -0300

    commit

---
+++ socotroco.js
@@ -0,0 +1,18 @@
+let stockProductos = [
+    {id: 1, nombre: "Comida para perro grande", tipo: "perro", cantidad: 1, desc: "Comida balanceada tamaño grande", precio: 1200, img:'./img/comidaPG.jpg'},
+    {id: 2, nombre: "Comida para perro pequeña", tipo: "perro", cantidad: 1, desc: "Comida balanceada tamaño pequeña", precio: 1100, img: './img/comidaPC.jpg'},
+    {id: 3, nombre: "Comida para gato grande", tipo: "gato", cantidad: 1, desc: "Comida balanceada tamaño grande", precio: 1200, img: './img/comidaG.jpg'},
+    {id: 4, nombre: "Comida para gato pequeña", tipo: "gato", cantidad: 1, desc: "Comida balanceada tamaño pequeña", precio: 1000, img: './img/comidaG.jpg'},
+    {id: 5, nombre: "Correa para perro", tipo: "perro", cantidad: 1, desc: "Correa reajustable para perros", precio: 1200, img: './img/correa.jpg'},
+    {id: 6, nombre: "Premio para mascotas", tipo: "general", cantidad: 1, desc: "Un bocadillo ideal para tu perris", precio: 900, img: './img/premios.jpg'},
+    {id: 7, nombre: "Arena para gato", tipo: "gatos", cantidad: 1, desc: "Arena hipoalergenica para gatos", precio: 500, img: './img/arenaGato.png'},
+    {id: 8, nombre: "Collar para gato", tipo: "gatos", cantidad: 1, desc: "Collar reajustable para gatos", precio: 500, img: './img/collarG.png'},
+    {id: 9, nombre: "Juguete para perro", tipo: "perros", cantidad: 1, desc: "Juguete duradero para perros en variedad de colores", precio: 200, img: './img/JugueteP.png'},
+    {id: 10, nombre: "Juguete para gato", tipo: "perros", cantidad: 1, desc: "Juguete super esponjoso para gatos con ruidos ", precio: 200, img: './img/jugueteG.jpg'},
+    {id: 11, nombre: "Cama para perros", tipo: "perros", cantidad: 1,desc: "Cama acolchonada para interior o exterior", precio: 2500, img: './img/camaMascota.png'},
+    {id: 12, nombre: "Cama para gatos", tipo: "gatos", cantidad: 1, desc: "Cama acolchonada para gatos con manta incluida", precio: 2200, img: './img/camaMascota.png'},
+    {id: 13, nombre: "Ropa para perro", tipo: "perros", cantidad: 1, desc: "Ropa abrigada de invierno para perros", precio: 1000, img: './img/ropaP.jpg'},
+    {id: 15, nombre: "Champu para animales", tipo: "general", cantidad: 1, desc: "Champú antipulgas para animales", precio: 1000, img: './img/champu.png'},
+    {id: 16, nombre: "Peine para animales", tipo: "general", cantidad: 1, desc: "Penine con cerdas suaves para animales", precio: 700, img: './img/peine.png'},
+    
+]
